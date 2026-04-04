d7a4d37b85f5464ff639fc9a1268282899e54855
Chloetrini <trinityegbukwu@yahoo.com>
2026-04-04 16:26:46 -0700

    corrections

--- src/Components/PropertiesDetails/MapView.tsx
+++ src/Components/PropertiesDetails/MapView.tsx
@@ -1,10 +1,19 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-// import L from "leaflet";
+import L from "leaflet";
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 import 'leaflet/dist/leaflet.css'; 
 import locationn from "/src/assets/location.png"
 
-// const containerstyle ={width:"100%", height: "400px"}
 
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: markerIcon2x,
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow,
+});
 interface MapProps {
   propertyName: string
   location: {fullAddress:string ,
